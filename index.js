ad08bb97adc3d0ad3f0e3a9cc34a2cefe54f939a
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-06-05 20:53:57 +0100

    constants-vault

---
+++ index.js
@@ -0,0 +1,16 @@
+let vault = {};
+
+exports.default = {
+    addGroup: (name, values) => {
+        vault[name] = values;
+    },
+    addToGroup: (groupName, key, value) => {
+        if(!vault[groupName]) vault[groupName] = {};
+        vault[groupName][key] = value;
+    },
+    getGroup: (groupName) => vault[groupName],
+    getGroupEntry: (groupName, key) => {
+        if(!vault[groupName]) throw new Error(`Group ${groupName} doesn't exist`);
+        else return vault[groupName][key];
+    }
+};
